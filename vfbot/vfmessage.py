7e97f0d3dc1cf6995fcf622fd874316892ff0bfd
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2025-04-28 23:47:54 +0100

    added option to use emoji as name

--- vfbot/vfmessage.py
+++ vfbot/vfmessage.py
@@ -9,6 +9,7 @@ class VFMessage:
         self.config = config
         
         self.target_channel_id = config['target_channel_id']
+        self.use_emoji = config.get('use_emoji_for_name', False)
         self.author_name = None
         self.credit = None
         self.embeds = []
@@ -38,9 +39,7 @@ class VFMessage:
             
         msg = cls(content.strip(), config)
         
-        msg.set_author_name(msg.config.get('author_name_override', ''))
-        if not msg.author_name:
-            msg.author_name = dc_msg.author.display_name
+        msg.set_author_name(config.get('author_name_override', dc_msg.author.display_name if not msg.use_emoji else f"{dc_msg.author.name}"))
         msg.credit = dc_msg.jump_url
         msg.set_embeds(dc_msg.embeds)
         return msg
@@ -49,7 +48,10 @@ class VFMessage:
     def content(self) -> str:
         _content = self._content
         if self.config.get('show_name', False):
-            _content = f"【{self.author_name}】{_content}"
+            if self.is_emoji(self.author_name):
+                _content = f"{self.author_name} {_content}"
+            else:
+                _content = f"【{self.author_name}】 {_content}"
         if self.config.get('show_credit', False):
             _content = f"{_content} | {self.credit}"
         return _content
@@ -60,11 +62,6 @@ class VFMessage:
                 self.embeds.append(embed)
                 
     def set_author_name(self, author_name: str):
-        # if author name is wrapped in $, replace it with the actual name
-        if not author_name.startswith("$") and author_name.endswith("$"):
-            self.author_name = author_name
-            return
-        
         if author_name == '$cathy-whale$':
             self.author_name = '巨鲸分析'
         elif author_name == '$cathy-swing$':
@@ -75,5 +72,16 @@ class VFMessage:
             self.author_name = '长线布局'
         elif author_name == '$cathy-strategy$':
             self.author_name = '交易心得'
+        else:
+            self.author_name = author_name
             
-            
+        if self.config.get('use_emoji_for_name', False) and not self.is_emoji(self.author_name):
+            emoji = self.config.get('emoji_mapping', {}).get(self.author_name, None)
+            if emoji:
+                self.author_name = emoji
+            else:
+                self.use_emoji = False # If no emoji is found, disable emoji for name
+                
+    @staticmethod
+    def is_emoji(val: str) -> bool:
+        return val.startswith("<:") and val.endswith(">")
