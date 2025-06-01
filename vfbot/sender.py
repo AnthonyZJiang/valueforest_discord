bd7e841e13131d8c88416e12aee2cc0b45aa6509
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2025-06-01 01:16:40 +0100

    added status message update

--- vfbot/sender.py
+++ vfbot/sender.py
@@ -1,10 +1,14 @@
 import asyncio
 import logging
 import datetime
+import json
+import os
 
 import discord
 from .vfmessage import VFMessage
 
+
+STATUS_MESSAGE_CONFIG_FILE = "status_message.config.json"
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +19,8 @@ class MessageSender(discord.Client):
         super().__init__(intents=intents)
         
         self.channels = {}  # type: dict[int, discord.TextChannel]
-        self.status_message = None  # Store the status message
-        self.status_update_task = None  # Store the background task
+        self.status_message = None  # type: discord.Message | None
+        self.status_update_task = None  # type: asyncio.Task | None
         
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
@@ -28,12 +32,20 @@ class MessageSender(discord.Client):
 
     async def on_ready(self):
         logger.info(f'Sender logged on as {self.user}')
+        
+        await self.load_status_message_from_cached_file()
+        if self.status_message:
+            asyncio.run_coroutine_threadsafe(self.start_status_updates(), self.loop)
 
-    async def send_message(self, message: VFMessage):
+    async def send_message(self, message: VFMessage) -> discord.Message:
         for channel_id in message.target_channel_ids:
             channel = self.get_cached_channel(channel_id)
             logger.info(f"Send message: Sending message to {channel.name}")
-            await channel.send(message.content, embeds=message.embeds)
+            return await channel.send(message.content, embeds=message.embeds)
+    
+    async def send_plain_message(self, content: str, channel_id: int) -> discord.Message:
+        channel = self.get_cached_channel(channel_id)
+        return await channel.send(content)
             
     async def delete_messages(self, message: discord.Message):
         if message:
@@ -68,22 +80,49 @@ class MessageSender(discord.Client):
     async def update_status_message(self):
         """Background task to update the status message every minute."""
         while True:
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            new_content = f"Bot is alive! Last update: {current_time}"
+            if not self.status_message:
+                await asyncio.sleep(5)
+                continue
+            current_time = int(datetime.datetime.now().timestamp())
+            new_content = f"机器人上次心跳报告: <t:{current_time}>, <t:{current_time}:R>"
             
             try:
-                await self.edit_message(1370721723241992215, self.status_message.channel.id, new_content)
+                await self.status_message.edit(content=new_content)
             except Exception as e:
                 logger.error(f"Error updating status message: {str(e)}")
             
             await asyncio.sleep(60)  # Wait for 1 minute
 
-    def start_status_updates(self, message: discord.Message):
+    async def start_status_updates(self):
         """Start periodic updates for the status message."""
-        self.status_message = message
         if self.status_update_task is None:
             self.status_update_task = asyncio.run_coroutine_threadsafe(
                 self.update_status_message(),
                 self.loop
             )
             logger.info("Started status message updates")
+    
+    async def create_status_message(self, channel_id: int):
+        self.status_message = await self.send_plain_message(
+            "机器人上次心跳报告: ...",
+            channel_id
+        )
+        if self.status_message:
+            self.save_status_message_to_cached_file()
+        
+    def save_status_message_to_cached_file(self):
+        with open(STATUS_MESSAGE_CONFIG_FILE, "w") as f:
+            json.dump({
+                "message_id": self.status_message.id,
+                "channel_id": self.status_message.channel.id,
+            }, f)
+            
+    async def load_status_message_from_cached_file(self):
+        if not os.path.exists(STATUS_MESSAGE_CONFIG_FILE):
+            return
+        with open(STATUS_MESSAGE_CONFIG_FILE, "r") as f:
+            data = json.load(f)
+            if not data['message_id'] and data['channel_id']:
+                await self.create_status_message(data['channel_id'])
+            else:
+                self.status_message = await self.get_cached_channel(data["channel_id"]).fetch_message(data["message_id"])
